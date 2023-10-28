1cb1cafa6bbffd01f9226590ac2eb54e021ecac8
HIMANSHU JAWLA <101585686+Codex-jawla@users.noreply.github.com>
2023-10-28 13:05:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import Textsphere from './components/Testsphere';
 import Particle from './components/Particle';
 
 function App() {
   return (
     <>
     <Particle/>
-      <Textsphere/>
     </>
   );
 }
